9edf9261372123b860a4ac52ae99760b7dba45fe
brandon-sandstrom <sandstromb@yahoo.com>
2017-09-26 12:19:39 -0600

    Update Class1-part1.php
    update beepboop api token

--- Class1-part1.php
+++ Class1-part1.php
@@ -1,4 +1,6 @@
-<?php
+curl -X GET -H "Authentication: Bearer <1905e73b0c47453ab36d3d2f6c75742f-1424767196." "https://beepboophq.com/api/v1/slack-teams";
+}
+<?php  
 $token = $_POST[ '0sZkDe3wGjudeOQcEgxJixFN' ];
 if ($token != '0sZkDe3wGjudeOQcEgxJixFN' ){
   $msg = "The token doesnt match";
