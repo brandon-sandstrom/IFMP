8919c5476471943c3fcec1ce3321d45b9249253c
brandon-sandstrom <sandstromb@yahoo.com>
2017-09-26 10:29:56 -0600

    Update Class1-part1.php

--- Class1-part1.php
+++ Class1-part1.php
@@ -1,12 +1,13 @@
 <?php
+$user_agent = "beepboophpforslack(https://beepboophq.com/proxy/df8f78fa88614ab48d35c4eee71d39fb/slack/command)";
 $token = $_POST[ '0sZkDe3wGjudeOQcEgxJixFN' ];
 if ($token != '0sZkDe3wGjudeOQcEgxJixFN' ){
   $msg = "The token doesnt match";
   die ($msg);
   echo $msg;
 }
-$user_name= $_POST[ 'sandstromb' ];
-if ($user_name != 'sandstromb' ){
+$channel_name = [ 'mentors' ];
+$user_list = [ 'name'];
    $msg = "you dont have access to this!"
    die ($msg);
    echo $msg;
